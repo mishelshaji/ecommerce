8fd41c4f39a11be0ed86f27a58a710ba2ddb6a44
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2021-01-06 17:29:15 +0530

    Complete login and registration page forms and design

--- ecommerce/accounts/forms.py
+++ ecommerce/accounts/forms.py
@@ -1,6 +1,42 @@
 from django import forms
+from django.db.models import fields
 from django.forms import widgets
 from .models import User, CustomerDetails
+
+class RegistrationForm(forms.ModelForm):
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control'
+            }
+        )
+    )
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password']
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'last_name': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'email': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'password': forms.PasswordInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+        }
+
 
 class CustomerDetailsForm(forms.ModelForm):
     class Meta:
--- ecommerce/accounts/views.py
+++ ecommerce/accounts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import CustomerDetailsForm
+from .forms import CustomerDetailsForm, RegistrationForm
 
 # Create your views here.
 def user_authenticate(request):
     if request.method == "GET":
         context = {}
-        context['form'] = AuthenticationForm()
+        context['form'] = RegistrationForm()
         context['detailsform'] = CustomerDetailsForm()
+        context['loginform'] = AuthenticationForm()
         return render(request, 'accounts/authenticate.html', context)
     
     
